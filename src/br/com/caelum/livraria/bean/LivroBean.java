275a5292588fcea6d00ba4b6c6ed3a614ae42f4a
LeonnyLino <leonny.1993@gmail.com>
2018-01-06 00:24:18 -0200

    - Modificou  o atributo dataLancamento de String para Calendar - moficou as demas classes para se adequar ao novo tipo do atributo - fez varias validações em jsf dentro dos arquivos xhtml.

--- src/br/com/caelum/livraria/bean/LivroBean.java
+++ src/br/com/caelum/livraria/bean/LivroBean.java
@@ -1,10 +1,14 @@
 package br.com.caelum.livraria.bean;
 
-import java.io.Serializable;
+
 import java.util.List;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
 
 import br.com.caelum.livraria.dao.DAO;
 import br.com.caelum.livraria.modelo.Autor;
@@ -12,7 +16,7 @@ import br.com.caelum.livraria.modelo.Livro;
 
 @ManagedBean
 @ViewScoped
-public class LivroBean implements Serializable {
+public class LivroBean {
 
 	private Livro livro = new Livro();
 	private Integer autorId;
@@ -46,11 +50,20 @@ public class LivroBean implements Serializable {
 		return new DAO<Livro>(Livro.class).listaTodos();
 	}
 	
+	public void comecaComDigitoUm(FacesContext fc, UIComponent component, Object value ) throws ValidatorException {
+		String valor = value.toString();
+		
+		if(!valor.startsWith("1")) {
+			throw new ValidatorException(new FacesMessage("Obrigatorio iniciar com 1!"));
+		}
+		
+	}
+	
 	public void gravar() {
 		System.out.println("Gravando livro: " + this.livro.getTitulo());
 
 		if (livro.getAutores().isEmpty()) {
-			throw new RuntimeException("Livro deve ter pelo menos um Autor.");
+			FacesContext.getCurrentInstance().addMessage("autor", new FacesMessage("Livro deve ter pelo menos um Autor"));
 		}
 
 		new DAO<Livro>(Livro.class).adiciona(this.livro);
